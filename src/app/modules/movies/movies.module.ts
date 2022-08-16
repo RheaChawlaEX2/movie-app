b1ac25f4f1632a22909843d16d85b5778278406b
Rhea Chawla <rchawla@ex2india.com>
2022-08-16 16:20:03 +0530

    done

--- src/app/modules/movies/movies.module.ts
+++ src/app/modules/movies/movies.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MovieCardListComponent } from './compo/movie-card-list/movie-card-list.component';
-import { MovieCardComponent } from './compo/movie-card/movie-card.component';
-import { WishlistComponent } from './compo/wishlist/wishlist.component';
+import { MovieCardListComponent } from './components/movie-card-list/movie-card-list.component';
+import { MovieCardComponent } from './components/movie-card/movie-card.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
 import { MoviesComponent } from './movies.component';
-import { MovieFilterComponent } from './compo/movie-filter/movie-filter.component';
+import { MovieFilterComponent } from './components/movie-filter/movie-filter.component';
 
 
 
