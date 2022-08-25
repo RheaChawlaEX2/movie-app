31f034a987f622a4cf6dfb9f215a828c3caf4332
Rhea Chawla <rchawla@ex2india.com>
2022-08-25 10:17:09 +0530

    npde update

--- src/app/modules/movies/movies.module.ts
+++ src/app/modules/movies/movies.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { MovieCardListComponent } from './components/movie-card-list/movie-card-list.component';
@@ -8,7 +7,6 @@ import { MovieCardComponent } from './components/movie-card/movie-card.component
 import { MovieFilterComponent } from './components/movie-filter/movie-filter.component';
 import { WishlistComponent } from './components/wishlist/wishlist.component';
 import { MoviesComponent } from './movies.component';
-import {HTTP_INTERCEPTORS} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -21,7 +19,6 @@ import {HTTP_INTERCEPTORS} from '@angular/common/http';
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
   ],
   exports: [
     MoviesComponent,
