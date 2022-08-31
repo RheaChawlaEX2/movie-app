46db526f41b33d94a700b20ff923135dc50cd87a
Rhea Chawla <rchawla@ex2india.com>
2022-08-31 15:41:36 +0530

    add routing and wishlist-lazy loading

--- src/app/modules/movies/movies.component.ts
+++ src/app/modules/movies/movies.component.ts
@@ -46,12 +46,7 @@ export class MoviesComponent implements OnInit, AfterContentChecked{
     else {
       this.wishlistComponent.clear()
     }
-    
-}
-
- 
-
-  
+} 
   ngAfterContentChecked(): void {
     this.wishListData = this.wishlist.getWishListData();
     this.count = this.wishlist.getWishListData().length
