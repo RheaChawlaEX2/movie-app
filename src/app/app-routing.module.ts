e51f811b0e3832a13850fdcba192d1dd099bcb79
Rhea Chawla <rchawla@ex2india.com>
2022-09-01 10:07:18 +0530

    stucture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddMovieFormComponent } from './modules/add-movie-form/add-movie-form.component';
-import { MoviesComponent } from './modules/movies/movies.component';
+import { AddMovieFormComponent } from './features/add-movie-form/add-movie-form.component';
+import { MoviesComponent } from './features/movies/movies.component';
 
 const routes: Routes = [
   { path: '', redirectTo: "movies" ,pathMatch:'full'},
   {path:'movies', component: MoviesComponent},
-  { path: 'movies/add', loadChildren:() => import('./modules/add-movie-form/add-movie-form.module').then(m => m.AddMovieFormModule) } 
+  { path: 'movies/add', loadChildren:() => import('./features/add-movie-form/add-movie-form.module').then(m => m.AddMovieFormModule) } 
 ];
 
 @NgModule({
